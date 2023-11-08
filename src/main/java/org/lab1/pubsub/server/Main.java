7bdf9f3e90e1b359319ed5dc422d7f96870dbddd
Daniel <2116528714@qq.com>
2023-11-08 16:48:10 +0800

    基本文件结构

---
+++ src/main/java/org/lab1/pubsub/server/Main.java
@@ -0,0 +1,10 @@
+package org.lab1.pubsub.server;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger("SERVER");
+    public static void main(String[] args) {
+        logger.info("Server started...");
+    }
+}
